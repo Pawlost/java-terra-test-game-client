f37058fd196e893f1f311a73bd6f3ce57ebb4c35
Pavel Balusek <mr.p.balusek@gmail.com>
2019-04-22 14:20:53 +0200

    Executable terra

---
+++ src/main/java/com/ritualsoftheold/testgame/WorldGenerator.java
@@ -0,0 +1,51 @@
+package com.ritualsoftheold.testgame;
+
+import com.ritualsoftheold.terra.core.buffer.BlockBuffer;
+import com.ritualsoftheold.terra.core.gen.interfaces.GeneratorControl;
+import com.ritualsoftheold.terra.core.gen.interfaces.world.WorldGeneratorInterface;
+import com.ritualsoftheold.terra.core.gen.tasks.GenerationTask;
+import com.ritualsoftheold.terra.core.gen.tasks.Pipeline;
+import com.ritualsoftheold.terra.core.material.MaterialRegistry;
+import com.ritualsoftheold.terra.core.material.TerraMaterial;
+import com.ritualsoftheold.terra.offheap.DataConstants;
+
+public class WorldGenerator implements WorldGeneratorInterface<Void> {
+
+    private TerraMaterial dirt;
+    private TerraMaterial air;
+    private TerraMaterial grass;
+    private int index;
+
+    @Override
+    public void setup(long seed, MaterialRegistry materialRegistry) {
+        dirt = materialRegistry.getMaterial("testgame:dirt");
+        grass = materialRegistry.getMaterial("testgame:grass");
+    }
+
+    @Override
+    public Void initialize(GenerationTask task, Pipeline<Void> pipeline) {
+        pipeline.addLast(this::generate);
+        return null;
+    }
+
+    public void generate(GenerationTask task, GeneratorControl control, Void nothing) {
+        BlockBuffer buf = control.getBuffer();
+        System.out.println("x:" + task.getX() + " y:" + task.getY() + " z:" + task.getZ());
+        if (task.getY() < 0) {
+            for (int i = 0; i < 2; i++) {
+                buf.next();
+            }
+            for (int i = 0; i < DataConstants.CHUNK_MAX_BLOCKS; i++) {
+                buf.write(dirt);
+                buf.next();
+            }
+        }else {
+            for (int i = 0; i < DataConstants.CHUNK_MAX_BLOCKS; i++) {
+                buf.write(grass);
+                buf.next();
+            }
+        }
+        index++;
+        System.out.println(index);
+    }
+}
